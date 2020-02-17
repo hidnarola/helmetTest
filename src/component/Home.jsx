a734b97291436249be73905d621d63e859ac32da
hidnarola <hda@narola.email>
2020-02-17 09:42:10 +0530

    minor change

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,8 +10,8 @@ class Home extends Component {
                     <title>Home Page From Helmet</title>
                     <meta name="description" content="Home Page from Helmet!" />
                     <meta name="theme-color" content="#008f68" />
-                    <meta name="title" content="Helmet Test Home | Join your friends in a social platform for friendly competition. Challenge and vote today!" />
-                    <meta property="og:title" content="Helmet Test Home | Join your friends in a social platform for friendly competition. Challenge and vote today!" />
+                    <meta name="title" content="Helmet Title Home | Join your friends in a social platform for friendly competition. Challenge and vote today!" />
+                    <meta property="og:title" content="Helmet Title Home | Join your friends in a social platform for friendly competition. Challenge and vote today!" />
                 </Helmet>
                 <Link to="/about">About</Link>
                 <h1>Inside Home</h1>
